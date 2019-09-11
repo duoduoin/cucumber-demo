e519b126f7ee3d80d6d9882ff018971471bb7ca1
Guiping Liu <Guiping_Liu@hotmail.com>
2019-09-11 21:56:18 +0800

    implement cucumber demo app

---
+++ src/test/java/com/gp/cucumber/controller/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package com.gp.cucumber.controller;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/resources")
+public class RunCucumberTest {
+
+}
